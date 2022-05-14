1a7aa193a1cf2925916f8636a7ad5737ecd079b0
calebnjw <lelebng@gmail.com>
2022-05-14 22:55:06 +0800

    working species index, all, edit, delete

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// CREATE TABLE [table] ([column] [type]...);
+// CREATE TABLE [table] ([column] [type], ...);
 // SELECT * FROM [table] (WHERE [column]=[value]);
 // SELECT * FROM [table] ORDER BY [column] ASC/DESC;
+// SELECT [table1.column], [table2.column]... FROM [table1]
+// >> INNER JOIN [table2] ON [table1.column]=[table2.column]
 // INSERT INTO [table] ([column], [column]) VALUES ([value], [value]);
 // UPDATE [table] SET [column]=[value] WHERE [column]=[value];
 // DELETE FROM [table] WHERE [column]=[value];
 // ALTER TABLE [table] RENAME [column] TO [newName];
+// ALTER TABLE [table] ADD [column] [type];
 
 // route syntax
 // app.get('/path/:parameter', (request, response) => {
@@ -66,7 +69,7 @@ const pool = new Pool(pgConnectionConfigs);
 app.get('/', (request, response) => {
   console.log('GET: ALL NOTES');
 
-  const query = 'SELECT * FROM notes ORDER BY id ASC;';
+  const query = 'SELECT * FROM notes ORDER BY note_id ASC;';
 
   pool.query(query)
     .then((result) => {
@@ -93,13 +96,13 @@ app.post('/note', (request, response) => {
   // get contents of form
   const note = JSON.parse(JSON.stringify(request.body));
   const {
-    habitat, date, appearance, behaviour, vocalisation, flock_size,
+    habitat, date, appearance, behaviour, vocalisation, flock_size, species_id,
   } = note;
 
   // send to database
   const query = `INSERT INTO notes 
-    (habitat, date, appearance, behaviour, vocalisation, flock_size) 
-    VALUES ('${habitat}', '${date}', '${appearance}', '${behaviour}', '${vocalisation}', '${flock_size}');`;
+    (habitat, date, appearance, behaviour, vocalisation, flock_size, species_id) 
+    VALUES ('${habitat}', '${date}', '${appearance}', '${behaviour}', '${vocalisation}', '${flock_size}', '${species_id}');`;
 
   pool.query(query)
     .then((result) => {
@@ -118,13 +121,13 @@ app.get('/note/:id', (request, response) => {
   console.log(`GET: NOTE ${id}`);
 
   // get contents of note
-  const query = `SELECT * FROM notes WHERE id=${id};`;
+  const query = `SELECT * FROM notes WHERE note_id=${id};`;
 
   pool.query(query)
     .then((result) => {
       const note = result.rows[0];
       // and display on page
-      response.render('note', { note });
+      response.render('note-view', { note });
     })
     .catch((error) => {
       console.log(error);
@@ -139,7 +142,7 @@ app.get('/note/:id/edit', (request, response) => {
   console.log(`GET: EDIT NOTE ${id}`);
 
   // get contents to fill up the form (same as /note/id)
-  const query = `SELECT * FROM notes WHERE id=${id};`;
+  const query = `SELECT * FROM notes WHERE note_id=${id};`;
 
   pool.query(query)
     .then((result) => {
@@ -161,7 +164,7 @@ app.put('/note/:id/edit', (request, response) => {
   // get contents of form
   const note = JSON.parse(JSON.stringify(request.body));
   const {
-    habitat, date, appearance, behaviour, vocalisation, flock_size,
+    habitat, date, appearance, behaviour, vocalisation, flock_size, species_id,
   } = note;
 
   // update values of table at specific id
@@ -171,8 +174,9 @@ app.put('/note/:id/edit', (request, response) => {
       appearance='${appearance}', 
       behaviour='${behaviour}', 
       vocalisation='${vocalisation}', 
-      flock_size = '${flock_size}' 
-    WHERE id=${id};`;
+      flock_size = '${flock_size}',
+      species_id = '${species_id}' 
+    WHERE note_id=${id};`;
 
   pool.query(query)
     .then((result) => {
@@ -189,7 +193,7 @@ app.delete('/note/:id/delete', (request, response) => {
   const { id } = request.params;
   console.log(`POST: DELETE NOTE ${id}`);
 
-  const query = `DELETE FROM notes WHERE id=${id};`;
+  const query = `DELETE FROM notes WHERE note_id=${id};`;
 
   pool.query(query)
     .then((result) => {
@@ -210,6 +214,151 @@ app.get('/species', (request, response) => {
   console.log('GET: NEW SPECIES FORM');
 
   response.render('species-new');
+});
+
+// send contents of new species to database
+app.post('/species', (request, response) => {
+  console.log('POST: NEW SPECIES FORM');
+
+  // get contents of form
+  const species = JSON.parse(JSON.stringify(request.body));
+  const { name, scientific_name } = species;
+
+  // send to database
+  const query = `INSERT INTO species 
+    (name, scientific_name) 
+    VALUES ('${name}', '${scientific_name}');`;
+
+  pool.query(query)
+    .then((result) => {
+      response.redirect('/');
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send(error);
+    });
+});
+
+// get all species
+app.get('/species/all', (request, response) => {
+  console.log('GET: ALL SPECIES');
+
+  // get all species from database
+  const query = 'SELECT * FROM species ORDER BY species_id ASC;';
+
+  pool.query(query)
+    .then((result) => {
+      const species = result.rows;
+      console.log(species);
+      response.render('species-all', { species });
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send(error);
+    });
+});
+
+// get notes from species
+app.get('/species/:id', (request, response) => {
+  const { id } = request.params;
+  console.log('GET: ALL NOTES FROM SPECIES');
+
+  const data = {};
+
+  // get name of species from database
+  const speciesQuery = `SELECT * FROM species WHERE species_id=${id};`;
+  pool.query(speciesQuery)
+    .then((result) => {
+      data.species = result.rows[0];
+      console.log('DATA SPECIES', data);
+    })
+    .then(() => {
+      // get notes from selected species
+      const query = `SELECT notes.note_id, 
+        notes.date, 
+        notes.habitat, 
+        species.species_id, 
+        species.name 
+        FROM notes 
+        INNER JOIN species
+        ON notes.species_id=species.species_id
+        WHERE notes.species_id=${id}
+        ORDER BY notes.note_id ASC;`;
+
+      pool.query(query)
+        .then((result) => {
+          data.notes = result.rows;
+          console.log('notes', data.notes);
+          response.render('species-view', { data });
+        })
+        .catch((error) => {
+          console.log(error);
+          response.send(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send(error);
+    });
+});
+
+// display edit species form
+app.get('/species/:id/edit', (request, response) => {
+  const { id } = request.params;
+  console.log('GET: EDIT SPECIES FORM');
+
+  // get name of species from database
+  const query = `SELECT * FROM species WHERE species_id=${id};`;
+  pool.query(query)
+    .then((result) => {
+      const species = result.rows[0];
+      response.render('species-edit', { species });
+    });
+});
+
+// send contents of new species to database
+app.put('/species/:id/edit', (request, response) => {
+  const { id } = request.params;
+  console.log('POST: EDITED SPECIES');
+
+  // get contents of form
+  const species = JSON.parse(JSON.stringify(request.body));
+  const { name, scientific_name } = species;
+
+  // send to database
+  const query = `UPDATE species 
+    SET name='${name}', 
+      scientific_name='${scientific_name}'
+    WHERE species_id=${id};`;
+
+  pool.query(query)
+    .then((result) => {
+      response.redirect('/species/all');
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send(error);
+    });
+});
+
+// delete species from database
+app.delete('/species/:id/delete', (request, response) => {
+  const { id } = request.params;
+  console.log('POST: EDITED SPECIES');
+
+  // delete from database
+  const query = `DELETE FROM species 
+    WHERE species_id=${id};`;
+
+  pool.query(query)
+    .then((result) => {
+      console.log('Deleted');
+      response.redirect('/species/all');
+    })
+    .catch((error) => {
+      console.log(error);
+      response.send(error);
+    });
 });
 
 // ------------------------------ //
